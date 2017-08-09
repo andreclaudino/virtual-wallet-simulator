16ed64d818839779956a381c058266657ca2cf60
André Claudino <andre.claudino@webradar.com>
2017-08-09 12:56:07 -0300

    #5-create-user-model
    Test for login with correct password

--- payment-system/tests/test_user.py
+++ payment-system/tests/test_user.py
@@ -9,26 +9,26 @@ class TestUser(TestCase):
     def setUp(self):
         ConnectDB.connect_database()
 
-        self.pwd = 'v3ry_h@rd_p@$$w0rd'
-        self.test = User(name="Testing User",
-                    username="test01",
-                    adress="0, Dummy Street, 219875-456",
-                    phone_number='+55 21 99999-999',
-                    mail_address='test@test_users.com',
-                    password=self.pwd)
-        self.test.save()
+        self.passwd = 'v3ry_h@rd_p@$$w0rd'
+        self.user = User(name="Testing User",
+                         username="test01",
+                         adress="0, Dummy Street, 219875-456",
+                         phone_number='+55 21 99999-999',
+                         mail_address='test@test_users.com',
+                         password=self.passwd)
+        self.user.save()
 
     def test_hashed_password(self):
         """
         Password should be hashed when set
         """
-        suid = str(self.test.uid)
-        a, b = factors(suid, self.pwd)
-        s = zip(suid * a, self.pwd * b)
+        suid = str(self.user.uid)
+        a, b = factors(suid, self.passwd)
+        s = zip(suid * a, self.passwd * b)
         p = ''.join([''.join(_) for _ in s]).encode('utf8')
         h = hashlib.sha384(p).hexdigest()
 
-        self.assertEqual(h, self.test.password)
+        self.assertEqual(h, self.user.password)
 
     def test_register_user_with_used_username(self):
         """
@@ -45,7 +45,7 @@ class TestUser(TestCase):
 
         user2.save()
 
-        result = len(User.nodes.filter(username=self.test.username))
+        result = len(User.nodes.filter(username=self.user.username))
         self.assertEqual(1, result)
 
     def test_register_user_with_used_username_return_none(self):
@@ -82,11 +82,18 @@ class TestUser(TestCase):
         self.assertEqual(user2.mail_address, 'user1@mail_adress.com')
 
     def test_login_correct_password(self):
-        pass
+        """
+        If user can access should return true
+        """
+        result = User.login('test01', self.passwd)
+        self.assertTrue(result)
 
     def test_login_incorrect_password(self):
         pass
 
+    def test_login_inactive_user(self):
+        pass
+
     def tearDown(self):
-        if self.test.save() is not None:
-            self.test.delete()
+        if self.user.save() is not None:
+            self.user.delete()
