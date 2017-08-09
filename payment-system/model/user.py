b9865a681a92f98cc3c87d1eef193868d3b39ccc
André Claudino <andre.claudino@webradar.com>
2017-08-09 14:10:32 -0300

    #5-create-user-model
    Increade methods documentation

--- payment-system/model/user.py
+++ payment-system/model/user.py
@@ -4,6 +4,9 @@ import hashlib
 from base.base_model import BaseModel
 
 class User(BaseModel):
+    """
+    This class models an user in database
+    """
     # Basic field names
     name = StringProperty(required=True)
     username = StringProperty(required=True, unique_index=True)
@@ -16,13 +19,28 @@ class User(BaseModel):
 
     @property
     def password(self):
+        """
+        Return hashed password
+        :return: hashed password
+        """
         return self.password_ if self.password_ else None
 
     @password.setter
     def password(self, value):
-        self.password_ = hash_pwd(self.uid, value)
+        """
+        Once password need to be hashed, this method/property is
+        used to hash entered password and save the hash instead
+        of clean one
+        """
+        self.password_ = mixture_pwd(self.uid, value)
 
     def save(self):
+        """
+        validates if an username is in use before save it
+        :return:
+            * Saved user object if it's ok
+            * None if username already in use
+        """
         # validate username
         if len(User.nodes.filter(username=self.username, uid__ne=self.uid)) == 0:
             return super(User, self).save()
@@ -45,14 +63,14 @@ class User(BaseModel):
         user = User.nodes.get_or_none(username=username)
         if user is None:
             return 'inexistent'
-        hash_passwd = hash_pwd(user.uid, passwd)
+        hash_passwd = mixture_pwd(user.uid, passwd)
         return (user.password_ == hash_passwd) if user.active else 'inactive'
 
 def factors(a_string,b_string):
     """
-    Given two numbers a, b, get the factors
-    they should be multiplied to arrive
-    common multiple
+    Given two strings a and b, get the factors
+    they should be multiplied to arrive same lenght
+    :return minimum factors to arrive lcm for a, and b, in this order
     """
     # TODO: Beter docstring
     a = len(a_string)
@@ -60,7 +78,14 @@ def factors(a_string,b_string):
     lcm =  int(abs(a * b) / math.gcd(a,b) if a and b else 0)
     return lcm // a, lcm // b
 
-def hash_pwd(base_k, val):
+def mixture_pwd(base_k, val):
+    """
+    This function is used to mixture a value (used a password) with
+    user id, then generate a hash who will be stored in database
+    :param base_k: base key to mixture val
+    :param val: value to be mixtured
+    :return: hashstring of mixtured values
+    """
     suid = str(base_k)
     value = str(val)
     a, b = factors(suid, value)
