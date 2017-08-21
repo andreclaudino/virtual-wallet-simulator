cebc0f96dc25f46264b829a0920c44743d153245
André Claudino <andre.claudino@webradar.com>
2017-08-21 17:30:41 -0300

    #6-create-wallet-model
    exceptions related to wallet

---
+++ payment-system/exceptions/wallet_exceptions.py
@@ -0,0 +1,32 @@
+class WalletException(Exception):
+    """
+    Base class for wallet exceptions
+    """
+    def __init__(self, message="Generic error with wallet module", *args, **kwargs):
+        super(WalletException, self).__init__(message, *args, **kwargs)
+        self.message = message
+
+
+class WalletLimitExceed(WalletException):
+    """
+    raised when wallet's real limit exceed maximum limit
+    """
+    def __init__(self, message="Real Limit exceeds maximum permited", *args, **kwargs):
+        super(WalletException, self).__init__(message, *args, **kwargs)
+        self.message = message
+
+class WalletLimitNotAllowed(WalletException):
+    """
+    raised when wallet limit is less than or equals to zero
+    """
+    def __init__(self, message="Changing Limit turns it less than or equals to zero", *args, **kwargs):
+        super(WalletException, self).__init__(message, *args, **kwargs)
+        self.message = message
+
+class UnchangebleWalletValue(WalletException):
+    """
+    raised when trying to change an unchangeble wallet value
+    """
+    def __init__(self, message="This value can not be changed directly", *args, **kwargs):
+        super(WalletException, self).__init__(message, *args, **kwargs)
+        self.message = message
