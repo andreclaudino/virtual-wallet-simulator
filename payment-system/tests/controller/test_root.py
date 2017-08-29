7b30003ec38b8a082db532d31ed06cca6cbd7858
André Claudino <andre.claudino@webradar.com>
2017-08-28 21:38:44 -0300

    #16-create-root-controller
    ## Tests for root controller
    * test_login_correct
    * test_login_incorrect_password
    * test_login_not_found_username
    * test_login_inactive_user

---
+++ payment-system/tests/controller/test_root.py
@@ -0,0 +1,109 @@
+from unittest.case import TestCase
+
+from flask import json
+
+from base.connect_db import ConnectDB
+from exceptions.user_exceptions import UserPasswordIncorrect, UsernameNotFound, UserInactive
+from model.user import User
+from utils.application_factory import create_app
+
+
+class RootTest(TestCase):
+
+    def setUp(self):
+        ConnectDB.connect_database()
+        app = create_app()
+        self.app = app.test_client()
+        self.app.testing = True
+
+        self.password = 'v3ry_h@rd_p@$$w0rd'
+        self.username = 'test01'
+
+        self.arguments = dict(name='Testing User',
+                         username=self.username,
+                         adress='0, Dummy Street, 219875-456',
+                         phone_number='+55 21 99999-999',
+                         mail_address='test@test_users.com',
+                         password=self.password)
+
+        # Create test user
+        result = self.app.post('/user', data=self.arguments, follow_redirects=True)
+        self.data = json.loads(result.get_data(as_text=True))
+
+        self.uid = self.data['uid']
+
+    def tearDown(self):
+        # Delete object
+        user = User.nodes.get_or_none(username=self.username)
+        if user:
+            user.delete()
+
+
+    def test_login_correct(self):
+        """
+        Test if login with correct credentials return
+        username and token
+        """
+
+        # Perform login
+        result = self.app.post('/login',
+                               data=dict(username=self.username, password=self.password),
+                               follow_redirects=True)
+        data = json.loads(result.get_data(as_text=True))
+
+        # Test login
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(data['user']['username'], self.username)
+        self.assertEqual(data['user']['uid'], self.uid)
+        self.assertIsNotNone(data['token'])
+
+    def test_login_incorrect_password(self):
+        """
+        Should return 401 on incorrect password
+        and match UserPasswordIncorrect message
+        """
+        # Perform login
+        result = self.app.post('/login',
+                               data=dict(username=self.username, password='wrong_password'),
+                               follow_redirects=True)
+        data = json.loads(result.get_data(as_text=True))
+
+        # Test login
+        self.assertEqual(result.status_code, 401)
+        self.assertEqual(data['error'], str(UserPasswordIncorrect()))
+
+    def test_login_not_found_username(self):
+        """
+        Should return 401 on incorrect username
+        and match UsernameNotFound message
+        """
+        # Perform login
+        result = self.app.post('/login',
+                               data=dict(username='wrong_username', password=self.password),
+                               follow_redirects=True)
+        data = json.loads(result.get_data(as_text=True))
+
+        # Test login
+        self.assertEqual(result.status_code, 401)
+        self.assertEqual(data['error'], str(UsernameNotFound()))
+
+    def test_login_inactive_user(self):
+        """
+        Should return 401 on login_in inactive
+        user and match UserInactive message
+        """
+
+        # Make user inactive
+        user = User.nodes.get_or_none(uid=self.uid)
+        user.active = False
+        user.save()
+
+        # Perform login
+        result = self.app.post('/login',
+                               data=dict(username=self.username, password=self.password),
+                               follow_redirects=True)
+        data = json.loads(result.get_data(as_text=True))
+
+        # Test login
+        self.assertEqual(result.status_code, 401)
+        self.assertEqual(data['error'], str(UserInactive()))
