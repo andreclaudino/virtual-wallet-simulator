83728da452a340a5ba7689781498d7a477dbef4d
André Claudino <andre.claudino@webradar.com>
2017-08-30 16:09:42 -0300

    heroku
    Suport for wsgi heroku in another initialization file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --pythonpath payment-system server:app --worker-class gevent -b 0.0.0.0:$PORT
+web: gunicorn --pythonpath payment-system server_wsgi:app --worker-class gevent -b 0.0.0.0:$PORT
---
+++ payment-system/server_wsgi.py
@@ -0,0 +1,17 @@
+from base.connect_db import ConnectDB
+from utils.application_factory import create_app
+
+# === Script to launch server ===
+
+"""
+This script launches payment-system server from configurations
+using configurations returned by server_config().
+"""
+
+
+ConnectDB.connect_database()
+
+app = create_app()
+
+if __name__=='__main__':
+    app.run()
