6547ceb68bb7754e6af21f3cfc45de54b2feed4e
André Claudino <andre.claudino@webradar.com>
2017-08-29 21:33:36 -0300

    #18-refactor-and-increase-documentation
    server docstring

--- payment-system/server.py
+++ payment-system/server.py
@@ -2,7 +2,14 @@ from base.connect_db import ConnectDB
 from utils.application_factory import create_app
 from utils.authorize import server_config
 
+# === Script to launch server ===
 
+"""
+This script launches payment-system server from configurations
+using configurations returned by server_config().
+
+It neeed certification files **cert.pem** and **key.pem** in launch directory
+"""
 
 if __name__ == "__main__":
 
