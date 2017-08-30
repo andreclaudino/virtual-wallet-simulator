5266a0006aa842efb16ef5f02a83b6466e1ea119
André Claudino <andre.claudino@webradar.com>
2017-08-29 21:34:09 -0300

    #18-refactor-and-increase-documentation
    Base controller docstring

--- payment-system/base/base_controller.py
+++ payment-system/base/base_controller.py
@@ -1,15 +1,13 @@
-"""
-BaseController
-
-this module contains the BaseController class. This class
-is a base wrapper of Flask Blueprint, making it easier to
-modularize endpoints and avoiding repeating configurations
-"""
-
 from flask import Blueprint
 
-
 class BaseController(Blueprint):
+    """
+    BaseController
+
+    This class is a base wrapper of Flask Blueprint, making
+    it easier to modularize endpoints and avoiding repeating
+    configurations
+    """
 
     def __init__(self, name=__name__, **kwargs):
         """
@@ -21,4 +19,7 @@ class BaseController(Blueprint):
         self.register_error_handler(404, self.handle_404)
 
     def handle_404(self, _):
+        """
+        Handle 404 errors not handled by other means
+        """
         return dict(message='Requested page not found'), 404
