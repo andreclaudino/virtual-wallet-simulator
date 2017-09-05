6e0b83523e8028c2c70415b4ada862997f94b669
André Claudino <andre.claudino@webradar.com>
2017-09-05 11:40:23 -0300

    wsgi-only
    Excluded standalone version. Now it's used only for development purpose.

--- payment-system/server.py
+++ payment-system/server.py
@@ -4,9 +4,7 @@ from utils.application_factory import create_app
 # === Script to launch server ===
 
 """
-This script launches payment-system server from configurations
-using configurations returned by server_config().
-Used to run as standalone app.
+This script launches payment-system server
 """
 
 
@@ -14,3 +12,5 @@ ConnectDB.connect_database()
 
 app = create_app()
 
+if __name__=='__main__':
+    app.run()
